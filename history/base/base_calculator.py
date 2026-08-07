c2d32f31cfcc19835c331b51a654adf1433feae0
Edgellence <info@edgellence.nl>
2026-08-07 09:40:51 +0000

    cleanup

--- history/base/base_calculator.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class HistoryCalculator(ABC):
-    @abstractmethod
-    def convert_history_to_platform_time(self, utc_timestamp: str) -> str:
-        """Convert UTC timestamp string to platform-local time string."""
-        pass
